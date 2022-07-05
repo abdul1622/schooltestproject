2a90ed52b9a687eca1c8518d7dd51a2616a72ae1
siva <sivasurlip@venzotechnologies.com>
2022-07-05 18:30:20 +0530

    5/7

--- academics/serializers.py
+++ academics/serializers.py
@@ -2,66 +2,35 @@ from dataclasses import field
 from rest_framework import serializers
 from .models import Grade,Subject,Chapter,Question,Answers
 questiontype_choice={
-
 ('MCQ','MCQ'),
-
 ('Fill_in_the_blanks','Fill_in_the_blanks'),
-
 ('Match_the_following','Match_the_following')
-
 }
 cognitive_level={
-
 ('Knowledge','Knowledge'),
-
 ('Comprehension','Comprehension'),
-
 ('Application','Application')
-
 }
 difficulty_level={
-
 ('Easy','Easy'),
-
 ('Medium','Medium'),
-
 ('Hard','Hard')
-
 }
-
-
 class GradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Grade
         fields = '__all__'
-
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = '__all__'
-
 class ChapterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chapter
         fields = '__all__'
-
 class ChapterViewSerializer(serializers.Serializer):
     grade = serializers.IntegerField()
     subject = serializers.CharField()
-# class AnswerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Answers
-#         fields=('option_a','option_b','option_c','option_d','answer')
-# class Questionserializer(serializers.ModelSerializer):
-#     answer=AnswerSerializer()
-#     class Meta:
-#         model=Question
-#         fields=['grade','subject','chapter','question','question_type','difficulty_level','cognitive_level','answer']
-#     def create(self,validated_data):
-#         ans=validated_data.pop('answer')
-#         q=Question.objects.create(**validated_data)
-#         Answers.objects.create(**ans)
-#         return q
 class Questionserializer(serializers.ModelSerializer):
     class Meta:
         model=Question
