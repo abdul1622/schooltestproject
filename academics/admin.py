c3acbfa3c57b2019551165217f26c334ac2aa84b
abdul1622 <abdulrahmans@venzotechnologies.com>
2022-08-10 14:57:19 +0530

    admin panel login page

---
+++ academics/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Grade,Subject,Chapter
+
+# Register your models here.
+
+class SubjectAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request,obj=None):
+        if request.user.is_anonymous:
+            return True
+        elif request.user.user_type == 'is_staff':
+            return False
+
+        elif request.user.user_type == 'is_admin':
+            return True
+
+class GradeAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request,obj=None):
+        if request.user.is_anonymous:
+            return True
+        elif request.user.user_type == 'is_staff':
+            return False
+
+        elif request.user.user_type == 'is_admin':
+            return True
+
+admin.site.register(Grade,GradeAdmin)
+admin.site.register(Subject,SubjectAdmin)
+admin.site.register(Chapter)
