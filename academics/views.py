8b14d89880b7805e98b7b1c6cc930149a668ba6e
abdul1622 <abdulrahmans@venzotechnologies.com>
2022-09-20 17:59:43 +0530

    question-paper

--- academics/views.py
+++ academics/views.py
@@ -521,7 +521,7 @@ class QuestionList(APIView):
                 if not status:
                     return Response({"status": "failure","data":"given details are incorrect"},status=HTTP_206_PARTIAL_CONTENT) 
                 serializer = QuestionPaperSerializer(question_paper)
-                return Response({'status':'success','data':serializer.data,'answer-file-path':'/media/answer_files/{answer_file}.pdf','subject_id':subject_obj.id,'grade_id':grade.id},status=HTTP_200_OK)
+                return Response({'status':'success','data':serializer.data,'question':questions,'answer-file-path':'/media/answer_files/{answer_file}.pdf','subject_id':subject_obj.id,'grade_id':grade.id},status=HTTP_200_OK)
             filename,status = render_to_pdf2('academics/question.html','question_paper',None,context)
             if not status:
                 return Response({"status": "failure","data":"given details are incorrect"},status=HTTP_206_PARTIAL_CONTENT) 
