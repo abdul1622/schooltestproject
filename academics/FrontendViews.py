6e50b1172880c9d37c8a5fd85f849ea4770d3706
siva <sivasurlip@venzotechnologies.com>
2022-09-22 10:21:54 +0530

    login

--- academics/FrontendViews.py
+++ academics/FrontendViews.py
@@ -8,7 +8,7 @@ import json
 from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url='/login')
+
 def gradeview(request): 
         if request.user.user_type =='is_admin' or request.user.user_type=='is_staff':
             gradeform = grade_form
@@ -16,7 +16,7 @@ def gradeview(request):
             chapterform = Chapterform
             return render(request,'academics/grade-subject-chapter.html',{'gradeform':gradeform,'subjectform':subjectform,'chapterform':chapterform})
         return render(request,'404.html')
-@login_required(login_url='/login')
+
 def questioncreationview(request): 
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff':
         form=question_form()
@@ -24,7 +24,6 @@ def questioncreationview(request):
         return render(request,'academics/question.html',{'form':form,'form1':form1})
     return render(request,'404.html')
 
-@login_required(login_url='/login')
 def chapterlistview(request):
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff':
         form= form_for_chapterlist()
@@ -32,28 +31,24 @@ def chapterlistview(request):
         return render(request,'academics/chapterlist.html',{'form':form,'form2':form2})
     return render(request,'404.html')
    
-@login_required(login_url='/login')
 def questionlistview(request):   
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff':
         form=questionlist_form()
         return render(request,'academics/questionlist.html',{'form':form})
     return render(request,'404.html')
     
-@login_required(login_url='/login')
 def chaptercrud(request):  
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff':
         form= chapter_form()
         return render(request,'academics/chapters.html',{'form':form})
     return render(request,'404.html')
    
-@login_required(login_url='/login')
 def subjectcrud(request): 
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff': 
         form= subject_form()
         return render(request,'academics/subjectcreate.html',{'form':form})
     return render('404.html')
 
-@login_required(login_url='/login')
 def questionview(request):
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff': 
         form = form_for_chapterlist(auto_id='get_%s')
@@ -62,7 +57,6 @@ def questionview(request):
         return render(request,'academics/questionandanswers.html',{'form':form,'answerform':answerform,'questionform':questionform})
     return render(request,'404.html')
    
-@login_required(login_url='/login')
 def question_paperview2(request):
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff': 
         form=questionlist_form()
@@ -73,7 +67,6 @@ def question_paperview2(request):
         return render(request,'academics/question_paper_custom.html',{'form':form,'list_form':list_form,'custom_form':custom_form,'form1':form1,'test_form':test_form})
     return render(request,'404.html')
     
-@login_required(login_url='/login')
 def question_paperview(request):
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff': 
         form=questionlist_form()
@@ -84,7 +77,6 @@ def question_paperview(request):
         return render(request,'academics/question_paper_customize.html',{'form':form,'list_form':list_form,'custom_form':custom_form,'form1':form1,'test_form':test_form})
     return render(request,'404.html')
   
-@login_required(login_url='/login')
 def questioncreate(request):
  
         if request.user.user_type =='is_admin' or request.user.user_type=='is_staff': 
@@ -93,7 +85,6 @@ def questioncreate(request):
             return render(request,'academics/question-by-grade.html',{'answerform':answerform,'questionform':questionform})
         return render(request,'404.html')
    
-@login_required(login_url='/login')
 def test_create(request):
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff': 
         form = form_for_chapterlist(auto_id='get_%s')
@@ -101,38 +92,32 @@ def test_create(request):
         return render(request,'academics/test-create.html',{'form':form,'test_form':test_form})
     return render(request,'404.html')
 
-@login_required(login_url='/login')
 def test_list(request):
     if request.user.user_type =='is_student': 
         return render(request,'academics/student-test-list-page.html')
     return render(request,'404.html')
   
-@login_required(login_url='/login')
 def testhistory(request):
     if request.user.user_type =='is_student': 
         return render(request,'academics/test-history.html')   
     return render(request,'404.html')
     
-@login_required(login_url='/login')
 def take_test(request,pk):
     if request.user.user_type =='is_student': 
         return render(request,'academics/take-test.html')
     return render(request,'404.html')
 
-@login_required(login_url='/login')
 def instruct(request):
     if request.user.user_type =='is_admin': 
         form = instruction_form()
         return render(request,'academics/instruction.html',{'form':form})
     return render(request,'404.html')
    
-@login_required(login_url='/login')
 def questionbank(request):
     if request.user.user_type =='is_student': 
         return render(request,'academics/question-bank.html')
     return render(request,'404.html')
  
-@login_required(login_url='/login')
 def student_test_history(request):
     if request.user.user_type =='is_admin' or request.user.user_type=='is_staff': 
         form = form_for_chapterlist()
