fcc5e645db087cfe1ebb664464def126a4e71be2
abdul1622 <abdulrahmans@venzotechnologies.com>
2022-10-10 10:15:50 +0530

    chapterlist

--- schoolproject/urls.py
+++ schoolproject/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIR)
+                              document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 urlpatterns += staticfiles_urlpatterns()
